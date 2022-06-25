835c604840bc7516fc3a552fa09743b116755bd9
yubrajthapa <yubrajthapa742@gmail.com>
2022-06-25 21:14:55 +0545

    Second commit

--- myapp/models.py
+++ myapp/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_lenght=100)
-      name = models.CharField(max_lenght=100)
-
+    name = models.CharField(max_length=100)
     price = models.IntegerField()
     desc = models.CharField(max_length=200)
+    phot = models.CharField(max_length=150, null=True)
       
