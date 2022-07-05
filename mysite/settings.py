651aaf99d8c402616d629abe4a6277c548025753
yubrajthapa <yubrajthapa742@gmail.com>
2022-07-05 21:58:45 +0545

    Adding images

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,5 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#for displaying an image
 MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.conf import settings
+# for displaying image import settings and static
 
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
  
 ]
 
+# displaying an image
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
