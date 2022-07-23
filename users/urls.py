63cf6406f7eea101d5bdca93e63539da391c12fa
yubrajthapa <yubrajthapa742@gmail.com>
2022-07-23 19:07:15 +0545

    Super User Login

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as authentication_view
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', views.register, name = 'register'),
+    path('login/', authentication_view.LoginView.as_view(template_name='users/login.html'),name="login"),
 
 ]
+
