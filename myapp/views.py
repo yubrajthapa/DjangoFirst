1258dc5bf5407808d78d8b71dc33b2021592d075
yubrajthapa <yubrajthapa742@gmail.com>
2022-10-29 19:55:22 +0545

    Creating seller profile

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from .models import Product
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
