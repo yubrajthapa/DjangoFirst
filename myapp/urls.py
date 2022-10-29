253dce824dbd418518c7a13b5e377649319602a4
yubrajthapa <yubrajthapa742@gmail.com>
2022-10-29 20:35:51 +0545

    Creating my listings

--- myapp/urls.py
+++ myapp/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     path('products/add/', views.add_product, name='add_product'),
     path('products/update/<int:id>/', views.update_product, name='update_product'),
     path('products/delete/<int:id>/', views.delete_product, name='delete_product'),
+    path('products/mylistings/', views.my_listings, name='mylistings'),
 ]
+ 
+
+
+
  
