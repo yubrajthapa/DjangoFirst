578f01a6dbe1eab481a3b877d7f949a6a85daac3
yubrajthapa <yubrajthapa742@gmail.com>
2022-10-30 19:09:33 +0545

    Class Based Update View

--- myapp/urls.py
+++ myapp/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     #path('products/add/', views.add_product, name='add_product'),
     path('products/add/', views.ProuctCreateView.as_view(), name='add_product'),
-    path('products/update/<int:id>/', views.update_product, name='update_product'),
+    #path('products/update/<int:id>/', views.update_product, name='update_product'),
+    path('products/update/<int:pk>/', views.ProductUpdateView.as_view(), name='update_product'),
     path('products/delete/<int:id>/', views.delete_product, name='delete_product'),
     path('products/mylistings/', views.my_listings, name='mylistings'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from .models import Product
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 # Create your views here.
 
@@ -71,6 +71,11 @@ def update_product(request,id):
         'product' : product
     }
     return render(request,'myapp/updateproduct.html',context)
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name','price','desc','image','seller_name']
+    template_name_suffix = '_update_form'
 
 def delete_product(request, id):
     product = Product.objects.get(id=id)
