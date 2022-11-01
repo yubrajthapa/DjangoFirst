29f3fa9e0fc4132e12735a2d34fea2faff1d04c5
yubrajthapa <yubrajthapa742@gmail.com>
2022-11-01 15:40:16 +0545

    Adding Custom Fields in Admin Panel

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.site_header = "Buy & Sell Website"
+admin.site.site_title = "Buy Best"
+admin.site.index_title = "Manage Buying Website"
+
 # Displays the 'id', 'name'of the product in admin pannel
-class ProductModelAdmin(admin.ModelAdmin):
-    list_display=["id", "name", "price"]
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'desc','id')
+
+admin.site.register(Product,ProductAdmin)
+
